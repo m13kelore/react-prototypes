53f5ffecce47cfa4112f51d2b378100134f348c8
m13kelore <kelceylorenzo@gmail.com>
2018-01-19 10:03:12 -0800

    React Intro - Kelcey Lorenzo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
-    document.getElementById('root')
-);
+const luckyNumber = () => {
+  return Math.ceil(Math.random() * 1000);
+};
+
+const greeting = user => {
+  let element = (
+    <div className="container">
+      <h1>Hello, {user.name}</h1>
+      <h2 className="text-muted">Your lucky number is: {user.luckyNumber}</h2>
+    </div>
+  );
+
+  return element;
+};
+
+let user = {
+  name: "Keith",
+  luckyNumber: luckyNumber()
+};
+
+ReactDOM.render(greeting(user), document.getElementById("root"));
