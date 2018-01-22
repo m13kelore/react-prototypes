0284fe2bf7dafb141b697c2c4c4d15dbb392f672
m13kelore <kelceylorenzo@gmail.com>
2018-01-22 15:51:04 -0800

    React Functional Components - Kelcey Lorenzo

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,31 @@
-import React from "react";
-import Table from "./table";
+import React from 'react';
+import Table from './table';
+
+const students = [
+	{
+		name: 'Takashi Shirogane',
+		course: 'Leadership',
+		grade: 91
+	},
+	{
+		name: 'Keith Kogane',
+		course: 'Being Cute',
+		grade: 100
+	},
+	{
+		name: 'Pidge Gunderson',
+		course: 'Care for Robots',
+		grade: 98
+	}
+];
 
 const App = () => {
-  return (
-    <div className="container">
-      <h1>Student Grade Table</h1>
-      <Table />
-    </div>
-  );
+	return (
+		<div className="container">
+			<h1>Student Grade Table</h1>
+			<Table data={students} />
+		</div>
+	);
 };
 
 export default App;
--- src/components/table.js
+++ src/components/table.js
@@ -1,34 +1,28 @@
-import React from "react";
+import React from 'react';
 
-const Table = () => {
-  return (
-    <table className="table">
-      <thead className="thead-inverse">
-        <tr>
-          <th>Name</th>
-          <th>Course</th>
-          <th>Grade</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Takashi Shirogane</td>
-          <td>Leadership</td>
-          <td>91</td>
-        </tr>
-        <tr>
-          <td>Keith Kogane</td>
-          <td>Being Cute</td>
-          <td>100</td>
-        </tr>
-        <tr>
-          <td>Pidge Gunderson</td>
-          <td>Care for Robots</td>
-          <td>98</td>
-        </tr>
-      </tbody>
-    </table>
-  );
+const Table = (props) => {
+	const tableRows = props.data.map((student, index) => {
+		return (
+			<tr key={index}>
+				<td>{student.name}</td>
+				<td>{student.course}</td>
+				<td>{student.grade}</td>
+			</tr>
+		);
+	});
+
+	return (
+		<table className="table">
+			<thead className="thead-inverse">
+				<tr>
+					<th>Name</th>
+					<th>Course</th>
+					<th>Grade</th>
+				</tr>
+			</thead>
+			<tbody>{tableRows}</tbody>
+		</table>
+	);
 };
 
 export default Table;
