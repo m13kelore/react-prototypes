4719ed349fb66176bb8c629a820498ca347540f8
m13kelore <kelceylorenzo@gmail.com>
2018-01-26 21:38:31 -0800

    completed up to feature set 7

---
+++ src/data/contacts.js
@@ -0,0 +1,27 @@
+import React from 'react';
+export default [
+	{
+		firstName: 'Keiji',
+		lastName: 'Akaashi',
+		phone: '(193)382-9293',
+		email: 'akaashikeiji@fukurodani.com'
+	},
+	{
+		firstName: 'Koutarou',
+		lastName: 'Bokuto',
+		phone: '(193)939-3949',
+		email: 'bokutokoutarou@fukurodani.com'
+	},
+	{
+		firstName: 'Shouyou',
+		lastName: 'Hinata',
+		phone: '(193)758-2039',
+		email: 'hinata.shouyou@karasuno.com'
+	},
+	{
+		firstName: 'Tobio',
+		lastName: 'Kageyama',
+		phone: '(193)943-6984',
+		email: 'kageyama.tobio@karasuno.com'
+	}
+];
