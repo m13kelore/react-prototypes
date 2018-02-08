ed1b06da43a575d15b0dd8e23b6dbf66cb37d0aa
m13kelore <kelceylorenzo@gmail.com>
2018-02-08 13:58:32 -0800

    finished axios prototype

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ import MovieContainer from './movie_container';
 export default (props) => {
 	return (
 		<div className="container">
-			<h1 className="text-center">Axios Demo</h1>
+			<h1 className="text-center">iTunes Top 10 - Movies</h1>
 			<MovieContainer />
 		</div>
 	);
--- src/components/movie.js
+++ src/components/movie.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default (props) => {
 	const { info } = props;
 	return (
-		<div>
+		<div className="col-xs-4">
 			<img src={info['im:image'][2].label} />
 			<h3>{info['im:name'].label}</h3>
 			<p>{info.summary.label}</p>
